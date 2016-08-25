1e0784b796222ff35368d348323cee22ffcebed3
avikdatta <avikd@ebi.ac.uk>
2016-08-25 21:13:35 +0100

    json dump with indent

--- lib/json_hub.py
+++ lib/json_hub.py
@@ -12,14 +12,18 @@ def file_dict(experiment,url_prefix):
     elif re.match(r'minusStrand', file_name, re.IGNORECASE):
       type='signal_reverse'
     else:
-      type='signal'
+      type='signal_unstranded'
   elif file_name.endswith('bb'):
       type='peak_calls'
-  browser_dict=defaultdict(dict)
-  browser_dict[type]['big_data_url']=url_prefix + experiment['FILE']
-  browser_dict[type]['md5sum']=experiment['FILE_MD5']
-  browser_dict[type]['primary']='true'
-  return browser_dict
+  else:
+      type='other'
+
+  file_url = url_prefix + experiment['FILE']
+  browser_dict = defaultdict(dict)
+  browser_dict['big_data_url']=file_url
+  browser_dict['md5sum']=experiment['FILE_MD5']
+  browser_dict['primary']=True
+  return browser_dict,type
 
 def read_index_file(infile, key_text):
   '''
@@ -37,7 +41,7 @@ def read_index_file(infile, key_text):
     header=[]
     file_list=defaultdict(list)
     for i in f:
-      row=i.split("\t")
+      row=i.rstrip('\n').split("\t")
       if(header):
         filtered_dict=dict(zip(header,row))
         exp_id=filtered_dict[key_text]
@@ -50,7 +54,11 @@ def read_index_file(infile, key_text):
   return file_list
 
 def required_attributes(type):
-  donor=['DONOR_ID', 'DONOR_AGE', 'DONOR_AGE_UNIT', 'DONOR_LIFE_STAGE','DONOR_HEALTH_STATUS', 'DONOR_SEX', 'DONOR_ETHNICITY']
+  donor=['DONOR_ID', 'DONOR_AGE', 'DONOR_AGE_UNIT', 'DONOR_LIFE_STAGE',
+         'DONOR_HEALTH_STATUS', 'DONOR_SEX', 'DONOR_ETHNICITY',
+        ]
+  meta= ['SAMPLE_ONTOLOGY_URI', 'MOLECULE', 'DISEASE', 'DISEASE_ONTOLOGY_URI', 'BIOMATERIAL_TYPE']
+
   required={ 'CELL_LINE' :['BIOMATERIAL_TYPE','LINE','LINEAGE','DIFFERENTIATION_STAGE','MEDIUM','SEX'],
              'PRIMARY_CELL': ['BIOMATERIAL_TYPE', 'CELL_TYPE'],
              'PRIMARY_TISSUE': ['BIOMATERIAL_TYPE','TISSUE_TYPE','TISSUE_DEPOT'],
@@ -62,17 +70,19 @@ def required_attributes(type):
     list.extend(donor)
   else:
     list=required[type]
+  list.extend(meta)
   return list
 
 def analysis_metadata(analysis):
   required=['ANALYSIS_GROUP','ALIGNMENT_SOFTWARE','ALIGNMENT_SOFTWARE_VERSION','ANALYSIS_SOFTWARE','ANALYSIS_SOFTWARE_VERSION'] 
-  analysis_dict=dict((k,v) for k,v in analysis.items() if k in required)
+  analysis_dict=dict((k.lower(),v) for k,v in analysis.items() if k in required)
   return analysis_dict
 
 def experiment_metadata(experiment):
-  required=['EXPERIMENT_TYPE','LIBRARY_STRATEGY','EXPERIMENT_ONTOLOGY_URI','REFERENCE_REGISTRY_ID']
-  exp_dict=dict((k,v) for k,v in experiment.items() if k in required)
-  exp_dict['EXPERIMENT_ONTOLOGY_URI']='-'
+  required=['EXPERIMENT_TYPE','EXPERIMENT_ONTOLOGY_URI','REFERENCE_REGISTRY_ID']
+  exp_dict=dict((k.lower(),v) for k,v in experiment.items() if k in required)
+  exp_dict['experiment_ontology_uri']='-'
+  exp_dict['assay']=experiment['LIBRARY_STRATEGY']
   return exp_dict
 
 def sample_metadata(sample):
@@ -89,6 +99,13 @@ def sample_metadata(sample):
     print('Unknown type: %s' % bio_type)
     sys.exit(2)
   required=required_attributes(type)
-  sample_dict=dict((k,v) for k,v in sample.items() if k in required)
+  sample_dict=dict((k.lower(),v) for k,v in sample.items() if k in required)
+
+  # JSON hub validator doesn't allow space in the age
+  if 'donor_age' in sample_dict:
+    sample_dict['donor_age']=re.sub(r'\s+','',sample_dict['donor_age'])
+
+  sample_dict['donor_life_stage']='unknown'
+  sample_dict['donor_age_unit']='year'
   return sample_dict
 
--- scripts/index_to_json_hub/convert_to_json.py
+++ scripts/index_to_json_hub/convert_to_json.py
@@ -1,21 +1,32 @@
-import os,sys
-import re
+import os,sys,re, json
 from collections import defaultdict
 from json_hub import read_index_file,required_attributes,experiment_metadata,sample_metadata,analysis_metadata, file_dict
 
-file_type_key='FILE_TYPE'
-exp_id_key='EXPERIMENT_ID'
-epirr_id_key='EPIRR_ID'
-sample_id_key='SAMPLE_ID'
-exp_id_key='EXPERIMENT_ID'
-url_prefix='http://ftp.ebi.ac.uk/pub/databases/'
+file_type_key = 'FILE_TYPE'
+file_key      = 'FILE'
+exp_id_key    = 'EXPERIMENT_ID'
+epirr_id_key  = 'EPIRR_ID'
+sample_id_key = 'SAMPLE_ID'
+exp_id_key    = 'EXPERIMENT_ID'
+url_prefix    = 'http://ftp.ebi.ac.uk/pub/databases/'
 
-index='/home/pi/python/pr7/work/test.index'
+index='/nfs/1000g-work/ihec/work/bp_pipe/dcc_work/2016/08_16/25_08_16/json_hub/public.results_GRCh38.index'
 index_data=read_index_file(index,exp_id_key)
-analysis_file='/home/pi/json_trackhub/trackhub/analysis_info.txt'
+analysis_file='/nfs/1000g-work/ihec/work/bp_pipe/dcc_work/2016/08_16/25_08_16/json_hub/analysis_info.txt'
 analysis_data=read_index_file(analysis_file,file_type_key)
-epirr_index='/home/pi/json_trackhub/trackhub/epirr_20160818.index'
+epirr_index='/nfs/1000g-work/ihec/work/bp_pipe/dcc_work/2016/08_16/25_08_16/json_hub/epirr_20160818.index'
 epirr_data=read_index_file(epirr_index,exp_id_key)
+
+
+hub_dict={
+    "assembly": "hg38", 
+    "date": "2016-08-16", 
+    "description": "Blueprint JSON Data hub generated for the IHEC Data Portal.", 
+    "email": "blueprint-info@ebi.ac.uk", 
+    "publishing_group": "Blueprint", 
+    "releasing_group": "Blueprint", 
+    "taxon_id": 9606
+   }
 
 samples_dict=defaultdict(dict)
 dataset_dict=defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
@@ -23,24 +34,28 @@ dataset_dict=defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 for exp,entries in index_data.items():
   sample_data=sample_metadata(entries[0])
   sample_id=entries[0][sample_id_key]
-  samples_dict['samples'][sample_id]=sample_data
+  samples_dict[sample_id]=sample_data
  
   for experiment in entries:
     file_type=experiment[file_type_key]
+    file_name=os.path.basename(experiment[file_key])
     exp_id=experiment[exp_id_key]
-    if file_type in analysis_data:          
+    if re.match(r'\.(bb|bw)$',file_name):
+   # if file_type in analysis_data:          
       '''
       Skip if file type is not required for trackhub
       '''
       exp_meta=experiment_metadata(experiment)
       analysis_meta=analysis_metadata(analysis_data[file_type][0])
-      browser_dict=file_dict(experiment,url_prefix)
+      (browser_dict,type)=file_dict(experiment,url_prefix)
       exp_meta[epirr_id_key]=epirr_data[exp][0][epirr_id_key]
+      
+      dataset_dict[exp_id]['analysis_attributes']=analysis_meta
+      dataset_dict[exp_id]['experiment_attributes']=exp_meta
+      dataset_dict[exp_id]['browser'][type].append(browser_dict) 
+      dataset_dict[exp_id]['sample_id']=sample_id    
 
-      dataset_dict['datasets'][exp_id]['analysis_attributes']=analysis_meta
-      dataset_dict['datasets'][exp_id]['experiment_attribute']=exp_meta
-      dataset_dict['datasets'][exp_id]['browser'].append(browser_dict)     
-
-print(dataset_dict)
+json_obj={ 'hub_description': hub_dict, 'datasets': dataset_dict, 'samples': samples_dict}
+print json.dumps(json_obj,indent=4)
 
 
