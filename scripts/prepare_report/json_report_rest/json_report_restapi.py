d4f6f7772842293483489becaef56f2a94cd1aa8
avikdatta <reach4avik@yahoo.com>
2016-10-05 13:14:42 +0000

    added support for host ip address

--- scripts/prepare_report/json_report_rest/json_report_restapi.py
+++ scripts/prepare_report/json_report_rest/json_report_restapi.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
+import argparse
 from flask import Flask
 from flask_restful import Api, Resource
 from blueprint.data.Blueprint_local_data import get_data
 from blueprint.index.Blueprint_index_stats import get_counts
+
+parser=argparse.ArgumentParser()
+parser.add_argument('-i','--host', default='127.0.0.1', help='REST api host ip')
+args=parser.parse_args()
+host=args.host
 
 app=Flask(__name__)
 api=Api(app)
@@ -17,5 +23,5 @@ class Json_report(Resource):
 api.add_resource(Json_report,'/json_stats')
 
 if __name__=='__main__':
-  app.run(debug=True)
+  app.run(host=host)
 
